1e4a8aed47e6871927a8923f54b7949b90985e8a
tathagat07 <tathagat.kumar3@gmail.com>
2019-09-24 16:27:01 +0530

    second commit

--- src/main/java/com/stackroute/domain/actor.java
+++ src/main/java/com/stackroute/domain/actor.java
@@ -2,6 +2,16 @@ package com.stackroute.domain;
 
 public class actor {
 
+    private String name;
+    private String gender;
+    private int age;
+
+    public actor(String name, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
@@ -13,10 +23,6 @@ public class actor {
     public int getAge() {
         return age;
     }
-
-    private String name;
-    private String gender;
-    private int age;
 
     public void setName(String name) {
         this.name = name;
